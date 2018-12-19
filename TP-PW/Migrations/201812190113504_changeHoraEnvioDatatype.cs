805014332e4de89da61cb9d197285b1f95a56ea0
Nuno Rocha <intersect.nerd@gmail.com>
2018-12-19 01:16:20 +0000

    Migration change HoraEnvio Datatype from time to datetime

---
+++ TP-PW/Migrations/201812190113504_changeHoraEnvioDatatype.cs
@@ -0,0 +1,18 @@
+namespace TP_PW.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changeHoraEnvioDatatype : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Mensagens", "HoraEnvio", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Mensagens", "HoraEnvio", c => c.Time(nullable: false, precision: 0));
+        }
+    }
+}
