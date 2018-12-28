d051922d0db68ba19d60c8ba79f9262acd59e328
Joao Monteiro <joao.monteiro95@hotmail.com>
2018-12-28 01:23:26 +0000

    Removed unnecessary includes

--- TP-PW/Models/EmprestimosViewModel.cs
+++ TP-PW/Models/EmprestimosViewModel.cs
@@ -1,10 +1,5 @@
 ﻿namespace TP_PW.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     public class EmprestimosViewModel
     {
